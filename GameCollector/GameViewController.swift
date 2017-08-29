2a27f9d6e26985d8e40b070f6c26262ced1ebb71
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-08-29 00:09:51 -0600

    Everything matching through video 7

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Travis Farnsworth on 8/28/17.
+//  Copyright © 2017 Travis Farnsworth. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+        
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
