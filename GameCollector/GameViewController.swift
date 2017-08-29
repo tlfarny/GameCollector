52a7eb7bc8539dd051356b62ff9aaba6d9437776
Travis Farnsworth <travis.l.farnsworth@gmail.com>
2017-08-29 00:16:30 -0600

    Everything working through 8

--- GameCollector/GameViewController.swift
+++ GameCollector/GameViewController.swift
@@ -27,6 +27,12 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         present(imagePicker, animated: true, completion: nil)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        gameImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
